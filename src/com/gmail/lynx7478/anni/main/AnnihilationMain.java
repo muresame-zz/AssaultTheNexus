669f49c195667e3fc8636be6412623fc3d686b3f
SKA4 <lynx7478@gmail.com>
2017-05-03 14:56:45 +0200

    Changed version.

--- src/com/gmail/lynx7478/anni/main/AnnihilationMain.java
+++ src/com/gmail/lynx7478/anni/main/AnnihilationMain.java
@@ -88,7 +88,7 @@ public class AnnihilationMain extends JavaPlugin implements Listener
 	}
 	
 	//TODO: Change this when updating.
-	private String version = "1.5.6";
+	private String version = "1.5.8";
 	
 	@Override
 	public void onEnable()
